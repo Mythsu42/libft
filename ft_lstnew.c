62a8d30b2e796963c9431c894a0bc968329c03f6
Mothsu <mothsuro@Nyxion.>
2025-05-16 17:30:10 +0200

    maj and add lstnew

---
+++ ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktolba <tolbakevin@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/16 00:25:50 by ktolba            #+#    #+#             */
+/*   Updated: 2025/05/16 01:06:30 by ktolba           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*lst;
+
+	lst = malloc(sizeof(t_list));
+	if (!lst)
+		return (NULL);
+	lst->content = content;
+	lst->next = NULL;
+	return (lst);
+}
--- ft_split.c
+++ ft_split.c
@@ -10,23 +10,46 @@ char **ft_split(char const *s, char c)
 	size_t	k;
 
 	sep = {c,'\0'};
-	str = ft_strtim(s,c);
+	str = ft_strtim(s,sep);
 	i = 0;
-	j = 0;
+	j = 1;
 	k = 0;
 	while (str[i])
 	{
-		if (str[i] == c || str[i] == '\0')
+		if (str[i] == c)
+			while (str[i] == c)
+				i++;
 			j++;
 		i++;
 	}
-	result = malloc(j + 1);
-	result[j] = '\0';
+	result = malloc(sizeof(char *) * ++j);
+	if (!result)
+		return (NULL);
+	result[j] = NULL;
 	i = 0;
 	j = 0;
-	while (str[i])
+	while (str[i] || str[i - 1])
 	{
-		while (str[i] != )
+		while (str[i] != c || str[i] != '\0')
+			i++;
+		result[j] = malloc(++i);
+		if (!result[j])
+			return (NULL);
+		j++;
+	}
+	i = 0;
+	j = 0;
+	while (str[i] || str[i - 1])
+	{
+		while (str[i] != c && str[i])
+		{
+			result[j][k] = str[i];
+			i++;
+			k++;
+		}
+		result[k] = '\0';
+		k = 0;
+		j++;
 	}
 	return (result);
 }
